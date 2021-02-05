093c03bb8f90f7f530072b5114ce9bd23efe0648
Björn Busse <bjoern.busse@kiwigrid.com>
2021-02-06 00:25:38 +0100

    Use Browser object

--- spotify-song-credits.py
+++ spotify-song-credits.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 #
 # spotify-song-credits
+# Scrape song credits from Spotify's web player
 #
 # Copyright (c) 2020 Björn Busse <bj.rn@baerlin.eu>
 #
@@ -24,39 +25,39 @@ path_logout = {
     "spotify":   ""
 }
 
-def spotify_song_credits():
+def spotify_song_credits(browser, login):
     # TODO: Use the WebDriverWait() function
     # https://selenium-python.readthedocs.io/waits.html
     time.sleep(15)
-    spotify_accept_cookies()
+    spotify_accept_cookies(browser)
     time.sleep(3)
     browser.get(login['url_payload'])
     time.sleep(15)
-    spotify_show_song_options()
+    spotify_show_song_options(browser)
     time.sleep(3)
-    spotify_show_credits()
+    spotify_show_credits(browser)
     time.sleep(3)
-    credits = spotify_get_credits()
+    credits = spotify_get_credits(browser)
     print(credits)
 
 
-def spotify_accept_cookies():
+def spotify_accept_cookies(browser):
     print("Accepting cookies")
     browser.find_element_by_id("onetrust-accept-btn-handler").click()
     return True
 
 
-def spotify_show_song_options():
+def spotify_show_song_options(browser):
     browser.find_element_by_xpath("//*[@aria-selected='true']/div/div[3]/button[2]").click()
     return True
 
 
-def spotify_show_credits():
+def spotify_show_credits(browser):
     browser.find_element_by_xpath("//*[text() = 'Show credits']/parent::button").click()
     return True
 
 
-def spotify_get_credits():
+def spotify_get_credits(browser):
     credits = {}
     credits['performed_by'] = browser.find_element_by_xpath("//*[text() = 'Performed by']/following::span").text
     credits['written_by'] = browser.find_element_by_xpath("//*[text() = 'Written by']/parent::div").text
@@ -67,5 +68,5 @@ def spotify_get_credits():
 
 
 if __name__ == '__main__':
-    wdu.browser_init(log_path, log_level, path_logout)
-    spotify_song_credits()
+    b = wdu.return_browser_init(log_path, log_level, path_logout)
+    spotify_song_credits(b.browser, b.login)
